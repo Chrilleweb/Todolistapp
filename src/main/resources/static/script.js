afde00209f5550d1ead4a40df6f0e54728f061b4
ChristianMN <christian.munknissen@gmail.com>
2023-02-19 23:14:45 +0100

    save locally

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,19 +2,24 @@ let textInput = document.querySelector(`form input[type="text"]`);
 let submitBtn = document.querySelector(`form input[type="submit"]`);
 let taskesDiv = document.querySelector(".taskes");
 
+// Retrieve tasks from localStorage on page load
+if (localStorage.getItem("tasks")) {
+    taskesDiv.innerHTML = localStorage.getItem("tasks");
+    removeBtn();
+}
+
 function createTheElement() {
     let ele = `
         <div class="task">
             ${textInput.value}
-            <buttom class="btn">Delete</buttom>
+            <button class="btn">Delete</button>
         </div>
-    `
-    return ele
+    `;
+    return ele;
 }
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    // console.log(textInput.value);
 
     if (textInput.value !== "") {
         taskesDiv.innerHTML += createTheElement();
@@ -25,16 +30,30 @@ submitBtn.addEventListener("click", (e) => {
 
     removeBtn();
 
-    // theStorage();
+    // Save tasks to localStorage
+    try {
+        localStorage.setItem("tasks", taskesDiv.innerHTML);
+        console.log("Tasks saved to localStorage:", localStorage.getItem("tasks"));
+    } catch (e) {
+        console.error("Error saving tasks to localStorage:", e);
+    }
 });
 
 function removeBtn() {
     let btn = document.querySelectorAll(".btn");
 
-    btn.forEach((buttom) => {
-        buttom.addEventListener("click", (e) => {
+    btn.forEach((button) => {
+        button.addEventListener("click", (e) => {
             e.preventDefault();
             e.target.parentElement.remove();
+
+            // Save tasks to localStorage after a task is deleted
+            try {
+                localStorage.setItem("tasks", taskesDiv.innerHTML);
+                console.log("Tasks saved to localStorage:", localStorage.getItem("tasks"));
+            } catch (e) {
+                console.error("Error saving tasks to localStorage:", e);
+            }
         });
     });
 };
